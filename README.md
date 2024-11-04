6ee06d4f84fe4ccfe3bc6a79aa5af130d60b6d45
CasarinJr <casarinjr@msn.com>
2024-11-03 22:47:55 -0300

    Added verbosity modes, search depth option, and result reports.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # CHECK DUPLICATES
 
 ## Description
-**Check_Duplicates** is a Bash script designed to help users manage duplicate files within a specified target directory (and its sub-directories).  It provides options to list, move or remove duplicates found based on user-defined criteria, making it faster and easier to organize files and free up disk space.
-It is useful for batch processing files, since the usage of metada-matches, filtering files before performing any actual data-match, speeds this process a lot!
+**Check_Duplicates** is a Bash script designed to help users manage duplicate files within a specified target directory (and its subdirectories).  It provides options to list, link, move or remove duplicates found based on user-defined criteria, making it faster and easier to organize files and free up disk space.
+It is useful for batch processing files, since the usage of metadata-matches, filtering files before performing any actual data-match, speeds this process a lot!
 
 ## Installation Instructions
 1. **Download the script**: Clone the repository or download the script file directly.
@@ -22,47 +22,62 @@ To run the script, use the following command format:
 ```
 
 ### Options:
+	Search operations (one):
+       [-r] Recursive search in target directory and all its subdirectories. (default)
+       [-d:] Maximum depth of search. Requires any number ≥ 1.
+
 	Match operations (any):
+       [-i] Perform a metadata match by inode (find hard links).
        [-s] Perform a metadata match by size in bytes. (always)
-       [-d] Perform a metadata match by date (last modification time).
-       [-i] Perform a metada match by inode (find hard links).
-       [-h] Perform a data match by bytes in head and tail.
+       [-t] Perform a metadata match by last modified time.
+       [-h] Perform a data match by 10 bytes in head and tail.
        [-c] Perform a data match by md5 checksum.
 
 	File operations (one):
        [-N] Files are not touched. (default)
        [-M] Move all duplicates found into target/DUPLICATE subdirectory.
        [-B] Move back files in target/DUPLICATE directory to its origin.
-       [-L] Link extra duplicates (replace files for hard links).
-       [-R] Remove extra duplicates found keeping just 1 master copy of each match.
+       [-L] Link extra duplicates (replace files by hard links).
+       [-R] Remove extra duplicates found, keeping just 1 master copy of each match.
+
+	Output verbosity:
+       [-q] Quiet: only status and operational messages are echoed in the terminal. (0 precedence)
+       [-n] Normal: final results are echoed additionally. (1 precedence) (default)
+       [-v] Verbose: partial results (of each matching process) are echoed additionally. (2 precedence)
 
 Combining options:
+ - Search operations cannot be combined, only one option is valid.
  - Match operations can be combined, any combination is valid.
  - File operations cannot be combined, only one option is valid.
+ - If combined, the higher output verbosity will take precedent.
 
 ### Examples:
-1. Checking files with same size and date in the ~/Downloads directory. Results are printed in the terminal.
+1. Checking files with same size and last modified time exclusively in the ~/Downloads directory (no search in subdirectories).
 ```bash
-./check_duplicates.sh -d ~/Downloads
+./check_duplicates.sh -t -d1 ~/Downloads
 ```
-2. Checking files with the same size, same modification time, same head and tail 10 bytes, and same checksum. Results are redirected to a text file for consultation.
+2. Checking files with the same size, same time, same 10 bytes at head and tail, and same checksum.
 ```bash
-./check_duplicates.sh -sdhc ~/Documents > duplicates.txt
+./check_duplicates.sh -sthc ~/Documents
 ```
-3. Moving all matches (size and headtail) to the DUPLICATES directory for analysis of the user.
+3. Moving all matches (size and headtail) to the DUPLICATES directory for analysis of the user. Quiet logs.
 ```bash
-./check_duplicates.sh -hM ~/Videos
+./check_duplicates.sh -qhM ~/Videos
 ```
-4. Removing all extra copies (same size, date and checksum) except for one master copy that is kept.
+4. Moving files back to their origin, after they were moved to the ~/Videos/DUPLICATES directory.
 ```bash
-./check_duplicates.sh -dR ~/Pictures/DUPLICATES
+./check_duplicates.sh -B ~/Videos
+```
+5. Removing all extra copies (same size, time, and checksum) except for one master copy that is kept. Verbose logs.
+```bash
+./check_duplicates.sh -vtR ~/Pictures/DUPLICATES
 ```
 
 ### Expected behavior and performance:
 
  1. **Size-match** is always performed, you don't really need to specify *-s* to perform it. It will always be the first thing done. This is the core principle for a speedy performance. No files can be exactly equal if they have different sizes, so there is no point on wasting resources processing a full data check when we can discard a match by a simple metadata check.
- 2. **Date-match** *-d* also speeds things a lot. However, it is optional because in a use case where files were modified back and forth, it would discard matches by different metadata even with the same content. When a user modifies the content of a copy, save it, then manually modify back the content as previous, saving it again. It will have a different "last modified time" in their metadata, even though in practice the content of the original and the copy are again the same.
- 3. **Inode-match** *-i* is only useful if you want to just see hard linked files. There is no need to use any other matching option with -i
+ 2. **Date-match** *-d* also speeds things a lot. However, it is optional because in some cases where files were opened and saved without modification or the modification was manually reverted, it would discard matches by presenting different metadata (last modified time) even with the same content (the actual data).
+ 3. **Inode-match** *-i* is useful if you want to manage your hard linked files. There is no need to use any other matching option with it, since files that have the same inode are the same. It is also useful for checking the success of the **Link-extras** operation *-L*. Since they do not occupy extra space, files hard linked (same inode) will not appear as duplicates unless this option is on.
  4. **Headtail-match** *-h* checks the first and last 10 bytes of a file. It is the fastest way to assess the actual data (not just the metadata) of a file. It performs fast too because it just checks two slices of the file, **independently of how big the file is**.  Although different files sometimes have the same start and ending (same type and style), when combined with metadata matches (size and date) they will return possible matches without overhead processing. Thus, it is useful for filtering big files before checking their data entirely.
  5. **Checksum-match** *-c* is currently the only full data analysis option available. It performs fast with small files and proportionally decreases in performance as the files increase in size. It is always the last check done, so when checking a lot of big files, it is useful to activate concomitantly other filters available to perform this action only on high probable matches.
  6. **Move-all** *-M* operation will rename files when moving them to the DUPLICATES directory to avoid rewriting over files from different origin subdirectories that would have the same name in the destiny directory. You SHOULD NOT simply delete all files moved to that folder, since then you would have no other copies left. That operation is designed for manual check and deletion, thus you should probably want to keep at least one copy of each duplicate pair or group found. The files are renamed with its previous path so you know where they came from, this way they will not be renamed/moved if their file path is longer than the maximum length allowed by your file system (typically 255 characters). You should not use this command multiple times in the same target directory, otherwise the renaming operation will build up for as many times as the operation is called.
--- check_duplicates.sh
+++ check_duplicates.sh
@@ -3,57 +3,84 @@
 # Author: Amauri Casarin Junior
 # Purpose: Find and move (optionally) probable duplicate files based on their metadata
 # License: GPL-3.0 license
-# Version: 0.7
-# Date: November 2, 2024
+# Version: 0.8
+# Date: November 3, 2024
 
-usage_message="Usage: $0 [-isdhc] [-N|M|B|R] target_directory
+usage_message="Usage: $0 [-options] target_directory
 
 Options:
+        Search operations (one):
+        [-r] Recursive search in target directory and all its subdirectories. (default)
+        [-d:] Maximum depth of search. Requires any number ≥ 1.
+
         Match operations (any):
-        [-i] Perform a metada match by inode (find hard links).
+        [-i] Perform a metadata match by inode (find hard links).
         [-s] Perform a metadata match by size in bytes. (default)
-        [-d] Perform a metadata match by date (last modification time).
-        [-h] Perform a data match by bytes in head and tail.
-        [-c] Perform a data match by md5
+        [-t] Perform a metadata match by last modified time.
+        [-h] Perform a data match by 10 bytes in head and tail.
+        [-c] Perform a data match by md5 checksum.
 
         File operations (one):
         [-N] Files are not touched. (default)
         [-M] Move all duplicates found into target/DUPLICATE subdirectory.
         [-B] Move back files in target/DUPLICATE directory to its origin.
-        [-L] Link extra duplicates (replace files for hard links).
-        [-R] Remove extra duplicates found keeping just 1 master copy of each match.
+        [-L] Link extra duplicates (replace files by hard links).
+        [-R] Remove extra duplicates found, keeping just 1 master copy of each match.
+
+        Output verbosity:
+        [-q] Quiet: only status and operational messages are echoed in the terminal. (0 precedence)
+        [-n] Normal: final results are echoed additionally. (1 precedence) (default)
+        [-v] Verbose: partial results (of each matching process) are echoed additionally. (2 precedence)
+
 
         Combinations:
+        Search operations cannot be combined, only one option is valid.
         Match operations can be combined, any combination is valid.
         File operations cannot be combined, only one option is valid.
+        If combined, the higher output verbosity will take precedent.
 
-Example: ./check_duplicates.sh -dcM ~/Documents"
+Example: ./check_duplicates.sh -tcM ~/Documents"
 
 # INITIALIZATION
 #--------------------------------------------------------------------------------------
+# SEARCH FLAGS
+RECURSIVE_SEARCH=1
+DEPTH_SEARCH=0
 # MATCH FLAGS
 SIZE_MATCH=true
 INODE_MATCH=false
-DATE_MATCH=false
+TIME_MATCH=false
 HEADTAIL_MATCH=false
 CHECKSUM_MATCH=false
-# FILE FLAGS AND VARIABLES
+# FILE FLAGS
 DO_NOTHING=1
 MOVE_ALL=0
 MOVE_BACK=0
 LINK_EXTRAS=0
 REMOVE_EXTRAS=0
-
+# OUTPUT VARIABLES
+verbosity=1 # 0 quiet, 1 normal and 2 verbose.
 #--------------------------------------------------------------------------------------
 
 #INPUT CHECKS
 #--------------------------------------------------------------------------------------
 # Process options using getopts
-while getopts ":sdihcNMBLR" opt; do
+while getopts ":rd:qnvstihcNMBLR" opt; do
     case "$opt" in
+        r) RECURSIVE_SEARCH=1;;
+
+        d) DEPTH_SEARCH=1; max_depth=${OPTARG}; RECURSIVE_SEARCH=0;;
+
+        q) verbosity=0;;
+
+        n) verbosity=1;;
+
+        v) verbosity=2 ;;
+
+
         s) SIZE_MATCH=true;;
 
-        d) DATE_MATCH=true;;
+        t) TIME_MATCH=true;;
 
         i) INODE_MATCH=true;;
 
@@ -74,14 +101,22 @@ while getopts ":sdihcNMBLR" opt; do
 
 
         \?) echo -e "Invalid option: -$OPTARG"; echo "$usage_message"; exit 1;;
+
+        :) echo "Option -$OPTARG requires an argument."
     esac
 done
 # Shift to remove processed options
 shift $((OPTIND - 1))
 
+
 # Check the correct number of arguments
 if [ "$#" -ne 1 ]; then
-    echo "Error: Invalid number of arguments."
+     echo "Error: Invalid number of arguments."
+     echo "$usage_message"
+     exit 1
+# Check conflicting search operations
+elif [ "$(( RECURSIVE_SEARCH + DEPTH_SEARCH ))" -gt 1 ]; then
+    echo "Error: Search operations -r and -d cannot be used together."
     echo "$usage_message"
     exit 1
 # Check conflicting file operations
@@ -100,22 +135,49 @@ else
 fi
 #--------------------------------------------------------------------------------------
 
+# Function to handle verbosity
+log() {
+    local message_type="$1"
+    # Capture echo options:
+    if [ $# -eq 3 ]; then
+        local echo_option="$2"
+        local message="$3"
+    else
+        local echo_option=""
+        local message="$2"
+    fi
+
+    # Echo message by verbosity level
+    if [ "$verbosity" = 0 ] && [ "$message_type" = "status" ]; then
+        echo "$echo_option" "$message"
+    elif  [ "$verbosity" = 1 ] && [[ "$message_type" =~ ^(status|final)$ ]]; then
+        echo "$echo_option" "$message"
+    elif  [ "$verbosity" = 2 ] && [[ "$message_type" =~ ^(status|partial|final)$ ]]; then
+        echo "$echo_option" "$message"
+    fi
+}
 
 # SEARCH FUNCTIONS
 #--------------------------------------------------------------------------------------
-find_all() {
-    echo -n "Getting metadata... "
+search_files() {
+    log status -n "Searching files... "
     # Find metadata for all files inside the directory
-    # fields: 1(size) 2(date) 3(headtail) 4(checksum) 5(inode) 6(path)
-    fields=6
-    TARGET_FILES="$(find "$TARGET_DIR" -type f -printf "%s\t%T+\t-\t-\t%i\t%p\n")"
+    # fields: 1(size) 2(time) 3(headtail) 4(checksum) 5(inode) 6(path)
+    duplicates_header="$(echo -e "Size\tTime\tHeadtail\tChecksum\tInode\tPath")"
+    if [ "$DEPTH_SEARCH" = 1 ]; then
+        TARGET_FILES="$(find "$TARGET_DIR" -maxdepth "$max_depth" -type f -size +0c -printf "%s\t%T+\t-\t-\t%i\t%p\n")"
+    else
+        TARGET_FILES="$(find "$TARGET_DIR" -type f -size +0c -printf "%s\t%T+\t-\t-\t%i\t%p\n")"
+    fi
     total_target="$(echo "$TARGET_FILES" | grep -c -v '^$')"
-
-    echo "ok"
+    log status "ok"
+    log status -e "Found files in $TARGET_DIR: $total_target\n"
 }
 
 
 find_duplicates() {
+    match_name="$2"
+    log status -E "Matching for $match_name..."
     DUPLICATES=$(awk -F'\t' -v cols="${match_columns[*]}" '
     BEGIN {
         split(cols, colsArr, " ")
@@ -140,12 +202,13 @@ find_duplicates() {
     }' <<< "$1" )
     total_duplicates="$(echo "$DUPLICATES" | grep -c -v '^$')"
     if [ "$total_duplicates" = 0 ];then
-        echo "$total_target files assessed. No duplicate files found for your criteria."
+        log status -e "No duplicate files found for your criteria."
         exit
     else
-        TARGET_FILES=$DUPLICATES #update the tarfet with the duplicates for each match operation
-        echo -e "Step duplicate files: $total_duplicates"
-        #echo "$DUPLICATES"
+        TARGET_FILES=$DUPLICATES #update the target with the duplicates for each match operation
+        log partial -E "$DUPLICATES" | sort -n
+        log status -E "Found $match_name-duplicates: $total_duplicates"
+        log partial ""
     fi
 }
 
@@ -170,32 +233,48 @@ find_extras() {
     EXTRA_DUPLICATES="$(echo "$DUPLICATES" | grep -v -x "$MASTER_DUPLICATES")"
     total_remove="$(echo "$EXTRA_DUPLICATES" | grep -c -v '^$')"
 
-    echo -e "\nMaster duplicate files to be kept:"
-    echo "$MASTER_DUPLICATES"
-    echo -e "Total: $total_keep"
-    echo -e "\nExtra duplicate files to be removed:"
-    echo "$EXTRA_DUPLICATES"
-    echo -e "Total: $total_remove"
+    log status -e "\nMaster duplicate files to be kept:"
+    log final -E "$MASTER_DUPLICATES"
+    log status -e "Total: $total_keep"
+    log final ""
+    log status -e "Extra duplicate files to be removed:"
+    log final -E "$EXTRA_DUPLICATES"
+    log status -e "Total: $total_remove"
+
+    master_duplicates_file="$TARGET_DIR/CDreport_master_duplicates.txt"
+    extra_duplicates_file="$TARGET_DIR/CDreport_extra_duplicates.txt"
+    echo "$duplicates_header" > $master_duplicates_file
+    echo "$duplicates_header" > $extra_duplicates_file
+    echo "$MASTER_DUPLICATES" >> $master_duplicates_file
+    echo "$EXTRA_DUPLICATES" >> $extra_duplicates_file
+    log status -e "\nMaster duplicates report saved to $master_duplicates_file"
+    log status -e "Extra duplicates report saved to $extra_duplicates_file"
 }
 
 
 find_matches() {
     # METADATA MATCH
     match_columns=()
-    if [ "$SIZE_MATCH" = true ];then
-        match_columns+=("1")
-        find_duplicates "$TARGET_FILES"
-    fi
-
-    if [ "$DATE_MATCH" = true ];then
-        match_columns+=("2")
-        find_duplicates "$TARGET_FILES"
-    fi
-
     if [ "$INODE_MATCH" = true ];then
         match_columns+=("5")
-        find_duplicates "$TARGET_FILES"
+        find_duplicates "$TARGET_FILES" "inode"
+    else
+        # Discard hard links (keep just one)
+        log status -E "Discarding hardlinked files"
+        DUPLICATES="$(awk -F'\t' '!seen[$5]++' <<< "$TARGET_FILES")"
+        TARGET_FILES=$DUPLICATES #update the target
     fi
+
+    if [ "$SIZE_MATCH" = true ];then
+        match_columns+=("1")
+        find_duplicates "$TARGET_FILES" "size"
+    fi
+
+    if [ "$TIME_MATCH" = true ];then
+        match_columns+=("2")
+        find_duplicates "$TARGET_FILES" "time"
+    fi
+
 
 
     # HEADTAIL-DATA MATCH
@@ -213,7 +292,7 @@ find_matches() {
         done <<< "$TARGET_FILES"
         echo -e "ok"
         TARGET_FILES="$(echo -e "$target_update")"
-        find_duplicates "$TARGET_FILES" "${match_columns[*]}"
+        find_duplicates "$TARGET_FILES" "headtail"
     fi
 
     # CHECKSUM-DATA MATCH
@@ -229,28 +308,37 @@ find_matches() {
         done <<< "$TARGET_FILES"
         echo -e "ok"
         TARGET_FILES="$(echo -e "$target_update")"
-        find_duplicates "$TARGET_FILES" "${match_columns[*]}"
+        find_duplicates "$TARGET_FILES" "checksum"
     fi
 
+    DUPLICATES="$(echo "$DUPLICATES" | sort -n)"
+    log status -e "\nResults:"
+    log final -E "$DUPLICATES"
+    log status -e "$total_target files assessed. $total_duplicates duplicates found."
 
-    echo -e "\nDuplicate files found:"
-    echo "$DUPLICATES" | sort -n
-    echo -e "$total_target files assessed. $total_duplicates duplicates found."
+    duplicates_file="$TARGET_DIR/CDreport_duplicates.txt"
+    echo "$duplicates_header" > $duplicates_file
+    echo "$DUPLICATES" >> $duplicates_file
+    log status -e "\nDuplicates report saved to $duplicates_file"
 }
 
-find_moved() {
+search_moved() {
     # Find metadata for all files inside the DUPLICATE directory
     MOVED_FILES="$(find "$DUPLICATES_DIR" -type f -printf "%p\n" | sort -n)"
     total_found="$(echo "$MOVED_FILES" | grep -c -v '^$')"
 
     if [ "$total_found" = 0 ];then
-        echo "No files found in $DUPLICATES_DIR."
+        log status "No files found in $DUPLICATES_DIR."
         exit
     fi
 
-    echo -e "Files found to be moved back:"
-    echo "$MOVED_FILES"
-    echo -e "Total: $total_found\n"
+    log status -e "Files found to be moved back:"
+    log partial -E "$MOVED_FILES"
+    log status -e "Total: $total_found\n"
+
+    moved_found="$TARGET_DIR/CDreport_moved_found.txt"
+    echo "$MOVED_FILES" > $moved_found
+    log status -e "\nMoved files found report saved to $moved_found"
 }
 
 #--------------------------------------------------------------------------------------
@@ -259,6 +347,7 @@ find_moved() {
 # FILE OPERATION FUNCTIONS
 #--------------------------------------------------------------------------------------
 confirm_action() {
+    echo ""
     read -p "Are you sure you want to $operation_name those files? [yes/no]: " response
     case "$response" in
         yes) return 0;;
@@ -270,8 +359,10 @@ confirm_action() {
 
 move_all() {
     counter=0
-    echo -e "\nMoving duplicates..."
     mkdir -p "$DUPLICATES_DIR"  # Create directory if it doesn't exist
+    log status -en "\nMoving duplicates..."
+    moved_files="$TARGET_DIR/CDreport_moved_files.txt"
+    echo -n "" > $moved_files
     while IFS=$'\t' read -r -a tabs; do
         counter=$((counter + 1))
         formatted_counter=$(printf "%0${#total_duplicates}d" "$counter") # counter with leading zeros
@@ -280,42 +371,60 @@ move_all() {
         relative_path=$(realpath --relative-to="$TARGET_DIR" "$path")
         new_filename="${formatted_counter} {:${relative_path//\//|}"  # Replace / with |
         mv "$path" "${DUPLICATES_DIR}/$new_filename"
-        echo "Moved $path to $DUPLICATES_DIR"
+        report_line="Moved $path to $DUPLICATES_DIR"
+        log partial "$report_line"
+        echo "$report_line" >> $moved_files
     done <<< "$TARGET_FILES"
-    echo "ok"
+    log status "ok"
+    log status -e "\nMoved files report saved to $moved_files"
 }
 
 move_back () {
-    echo -e "\nMoving files back..."
+    log status -en "\nMoving files back..."
+    movedback_files="$TARGET_DIR/CDreport_movedback_files.txt"
+    echo -n "" > $movedback_files
     while IFS=$'\t' read -r currernt_path; do
         formatted_old_path="${currernt_path#*\{:}"  # Remove everything up to the first '{:'
         old_path="${TARGET_DIR}/${formatted_old_path//\|//}"  # Replace back all '|' with '/'
         mv "$currernt_path" "$old_path"
-        echo "Moved to $old_path"
+        report_line="Moved back to $old_path"
+        log partial "$report_line"
+        echo "$report_line" >> $movedback_files
     done <<< "$MOVED_FILES"
-    echo "ok"
+    log status -E "ok"
+    log status -e "\nMoved back files report saved to $movedback_files"
 }
 
 link_extras() {
-    echo -e "\nReplacing extra duplicates by links..."
+    log status  -en "\nReplacing extra duplicates by hard links..."
+    linked_files="$TARGET_DIR/CDreport_linked_files.txt"
+    echo -n "" > $linked_files
     while IFS=$'\t' read -r -a tabs; do
         checksum="${tabs[0]}"
         link_path="${tabs[-1]}"
         master_path=$(echo "$MASTER_DUPLICATES" | grep -E "^($checksum)" | awk -F'\t' '{print $NF}')
         ln -Tf "$master_path" "$link_path" # forcing (-f) the link to replace the duplicates
-        echo "Replaced "$link_path" by a link to "$master_path""
+        report_line="Replaced $link_path by a link to $master_path"
+        log partial "$report_line"
+        echo "$report_line" >> $linked_files
     done <<< "$EXTRA_DUPLICATES"
-    echo "ok"
+    log status "ok"
+    log status -e "\nHard linked files report saved to $linked_files"
 }
 
 remove_extras() {
-    echo -e "\nRemoving extra duplicates..."
+    log status  -e "\nRemoving extra duplicates..."
+    removed_files="$TARGET_DIR/CDreport_removed_files.txt"
+    echo -n "" > $removed_files
     while IFS=$'\t' read -r -a tabs; do
         path="${tabs[-1]}"
         rm "$path"
-        echo "Removed $path"
+        report_line="Removed $path"
+        log partial "$report_line"
+        echo "$report_line" >> $removed_files
     done <<< "$EXTRA_DUPLICATES"
-    echo "ok"
+    log status "ok"
+    log status -e "\nRemoved files report saved to $removed_files"
 }
 #--------------------------------------------------------------------------------------
 
@@ -323,33 +432,35 @@ remove_extras() {
 # OPERATION:
 #--------------------------------------------------------------------------------------
 if [ "$DO_NOTHING" = 1 ]; then
-    find_all
+    search_files
     find_matches
     exit
 elif [ "$MOVE_ALL" = 1 ]; then
-    find_all
+    search_files
     find_matches
     confirm_action
     move_all
     exit
 elif [ "$LINK_EXTRAS" = 1 ]; then
-    find_all
+    search_files
     find_matches
     find_extras
     confirm_action
     link_extras
     exit
 elif [ "$REMOVE_EXTRAS" = 1 ]; then
-    find_all
+    search_files
     find_matches
     find_extras
     confirm_action
     remove_extras
     exit
 elif [ "$MOVE_BACK" = 1 ]; then
-    find_moved
+    search_moved
     confirm_action
     move_back
     exit
 fi
 #--------------------------------------------------------------------------------------
+
+
